f6b17cc97f46e67184e15784ddd91d297eddb2d6
alanshasalim <alanthegamer@gmail.com>
2022-07-14 09:50:15 +0400

    first commit

---
+++ dvl-viewportchecker.js
@@ -0,0 +1,17 @@
+// DVL Viewportchecker 1.0
+// Developed by DevilCoder
+// https://github.com/devilcoder181
+
+// Options
+// activeclass: 'active-section'
+// endclass: 'end-section'
+// offset: 0
+// classback: {
+//     init: ()=>{},
+//     onenter: ()=> {},
+//     onleave: ()=> {}
+// }
+
+class DVLViewportchecker {
+    
+}
